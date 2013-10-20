24c4853fc402a82dc72c77b1e27836948dc71e02
cameron581 <cameron581@cox.net>
2013-10-19 20:11:41 -0500

    Update u8815.mk

--- u8815.mk
+++ u8815.mk
@@ -24,7 +24,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from u8815 device
-$(call inherit-product, device/huawei/u8815/device.mk)
+$(call inherit-product, device/huawei/u8665/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := huawei_u8665
