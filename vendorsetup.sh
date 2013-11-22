f37744bab6e9d33e1142e48576f81ed2ba5bfcb1
cameron581 <cameron581@cox.net>
2013-11-22 08:31:33 -0600

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 if [ ! -z "$JENKINS_HOME" ]
 then
   # If we're Jenkins, run device patches on the tree. I'm lazy and this is easy for nightly builds.
-  sh device/huawei/u8815/patches/apply.sh
+  sh device/huawei/u8665/patches/apply.sh
 fi
